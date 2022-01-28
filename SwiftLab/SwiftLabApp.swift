f24a0ec77c38e6d6a144e9479624f2d68131762b
Prayag Gediya <prayaggediya60@gmail.com>
2022-01-28 15:41:59 +0530

    Initial Commit

---
+++ SwiftLab/SwiftLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLabApp.swift
+//  SwiftLab
+//
+//  Created by PC on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
